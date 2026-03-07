0123f6848a0a00eb51e7cbd15bda2bd24f4e86e1
Dhruv <Skullgame300@gmail.com>
2026-03-06 18:53:43 -0800

    Little Code

--- rag_database_routing.py
+++ rag_database_routing.py
@@ -1,1 +1,25 @@
-"MY CODE"
+def init_session_state():
+    """Initialize session state variables"""
+    if 'openai_api_key' not in st.session_state:
+        st.session_state.openai_api_key = ""
+    if 'qdrant_url' not in st.session_state:
+        st.session_state.qdrant_url = ""
+    if 'qdrant_api_key' not in st.session_state:
+        st.session_state.qdrant_api_key = ""
+    if 'embeddings' not in st.session_state:
+        st.session_state.embeddings = None
+    if 'llm' not in st.session_state:
+        st.session_state.llm = None
+    if 'databases' not in st.session_state:
+        st.session_state.databases = {}
+
+init_session_state()
+
+DatabaseType = Literal["products", "support", "finance"]
+PERSIST_DIRECTORY = "db_storage"
+
+@dataclass
+class CollectionConfig:
+    name: str
+    description: str
+    collection_name: str  # This will be used as Qdrant collection name
